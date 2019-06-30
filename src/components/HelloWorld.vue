e878cea89ced52fee587aeab403e28adc91d786c
Andrew <kungpaogao@gmail.com>
2019-06-30 01:35:59 -0400

    Added data types for Furniture and Clients

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,22 @@
   <v-container>
     <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
       </v-flex>
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          <br />please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank"
+            >Discord Community</a
+          >
         </p>
       </v-flex>
 
@@ -27,7 +31,8 @@
             :href="next.href"
             class="subheading mx-3"
             target="_blank"
-          >{{ next.text }}</a>
+            >{{ next.text }}</a
+          >
         </v-layout>
       </v-flex>
 
@@ -41,7 +46,8 @@
             :href="link.href"
             class="subheading mx-3"
             target="_blank"
-          >{{ link.text }}</a>
+            >{{ link.text }}</a
+          >
         </v-layout>
       </v-flex>
 
@@ -55,7 +61,8 @@
             :href="eco.href"
             class="subheading mx-3"
             target="_blank"
-          >{{ eco.text }}</a>
+            >{{ eco.text }}</a
+          >
         </v-layout>
       </v-flex>
     </v-layout>
---
+++ src/data/ClientType.ts
@@ -0,0 +1,27 @@
+export type Client = {
+  phoneNumber: string;
+  address: string;
+  zone: string;
+  dateConfirmed?: Date;
+  dateDelivered?: Date;
+
+  requests: {
+    beds: number;
+    sofa: number;
+    diningSet: number; // number of chairs needed
+    otherTables: number;
+    dresser: number;
+    crib: number;
+  };
+  conditions: {
+    leavingHomelessness: boolean;
+    recentPrison: boolean;
+    pregnantMember: boolean;
+    under18Children: boolean;
+    disability: boolean;
+    veteran: boolean;
+    elderly: boolean;
+    domesticViolence: boolean;
+  };
+  comments: string;
+};
