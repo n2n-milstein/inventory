a0a06fe6d61f14d18fa208b7029a8e91a56cd12e
jweed4033 <jweed403@outlook.com>
2019-12-09 13:18:57 -0500

    Implemented InventoryActions, just add/archive button for now

---
+++ src/components/InventoryActions.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout px-3 mb-3>
+    <v-btn color="primary">
+      <v-icon>add</v-icon>
+      Add
+    </v-btn>
+    <v-btn icon>
+      <v-icon>archive</v-icon>
+    </v-btn>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component({})
+export default class InventoryActions extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
