ebd8769c75323f84df055aff507da4d0488d2561
Andrew <kungpaogao@gmail.com>
2020-02-23 19:07:29 -0500

    Implemented UI change on scroll; updated spacing

--- src/components/EditCard.vue
+++ src/components/EditCard.vue
@@ -1,148 +1,166 @@
 <template>
   <v-card>
-    <v-card-title class="headline" primary-title>
+    <v-card-title
+      class="headline ease-transition pb-4"
+      :class="{
+        'elevation-3': offsetTop > 0,
+        'no-line': offsetTop > 0,
+        'elevation-0': offsetTop === 0,
+        line: offsetTop === 0,
+      }"
+      primary-title
+    >
       {{ isEdit ? "Edit" : "Add" }} Furniture
     </v-card-title>
 
-    <v-card-text>
-      <!-- <v-container> -->
-      <v-row>
-        <v-col cols="12">
-          <v-form ref="edit-form" v-model="valid" lazy-validation>
-            <!-- Donor Info -->
-            <h2>Donor Info</h2>
+    <v-card-text id="scroll-target" class="pa-0">
+      <v-container>
+        <v-row v-scroll:#scroll-target="onScroll" class="px-4">
+          <v-col cols="12">
+            <v-form ref="edit-form" v-model="valid" lazy-validation>
+              <!-- Donor Info -->
+              <h2>Donor Info</h2>
 
-            <v-text-field
-              v-model="donorName"
-              :rules="required"
-              label="Donor Name"
-              required
-              prepend-icon="person"
-            />
+              <v-text-field
+                v-model="donorName"
+                :rules="required"
+                label="Donor Name"
+                required
+                prepend-icon="person"
+              />
 
-            <v-text-field
-              v-model="phone"
-              :rules="required"
-              label="Phone Number"
-              required
-              prepend-icon="phone"
-            />
+              <v-text-field
+                v-model="phone"
+                :rules="required"
+                label="Phone Number"
+                required
+                prepend-icon="phone"
+              />
 
-            <v-text-field
-              v-model="email"
-              :rules="emailRules"
-              label="Email"
-              required
-              prepend-icon="email"
-            />
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="Email"
+                required
+                prepend-icon="email"
+              />
 
-            <v-text-field
-              v-model="address"
-              :rules="required"
-              label="Address"
-              required
-              prepend-icon="location_on"
-            />
+              <v-text-field
+                v-model="address"
+                :rules="required"
+                label="Address"
+                required
+                prepend-icon="location_on"
+              />
 
-            <v-text-field
-              v-model="zone"
-              :rules="required"
-              label="Zone"
-              required
-            />
+              <v-text-field
+                v-model="zone"
+                :rules="required"
+                label="Zone"
+                required
+              />
 
-            <v-divider class="my-3" />
+              <v-divider class="my-3" />
 
-            <!-- Physical Attributes -->
-            <h2>Physical Attributes</h2>
+              <!-- Physical Attributes -->
+              <h2>Physical Attributes</h2>
 
-            <h3 v-if="!fclass">
-              Select a furniture class
-            </h3>
+              <h3 v-if="!fclass">
+                Select a furniture class
+              </h3>
 
-            <v-select
-              v-model="fclass"
-              :items="classOptions"
-              :rules="[(v) => !!v || 'Item is required']"
-              label="Furniture Class"
-              required
-              prepend-icon="category"
-            />
+              <v-select
+                v-model="fclass"
+                :items="classOptions"
+                :rules="[(v) => !!v || 'Item is required']"
+                label="Furniture Class"
+                required
+                prepend-icon="category"
+              />
 
-            <physical-attr :fclass="fclass" />
+              <physical-attr :fclass="fclass" />
 
-            <v-divider class="my-3" />
+              <v-divider class="my-3" />
 
-            <!-- Attributes -->
-            <h2>Attributes</h2>
+              <!-- Attributes -->
+              <h2>Attributes</h2>
 
-            <attribute-question
-              v-for="attr in attributes"
-              :key="attr"
-              :attribute="attr"
-              @answer="updateAttr(attr, $event)"
-            />
+              <attribute-question
+                v-for="attr in attributes"
+                :key="attr"
+                :attribute="attr"
+                @answer="updateAttr(attr, $event)"
+              />
 
-            <v-divider class="my-3" />
+              <v-divider class="my-3" />
 
-            <!-- Timing -->
-            <h2>Timing</h2>
+              <!-- Timing -->
+              <h2>Timing</h2>
 
-            <date-picker-menu
-              label="Date Offered"
-              @date="dateOffered = $event"
-            />
+              <date-picker-menu
+                label="Date Offered"
+                @date="dateOffered = $event"
+                spacing="pb-3"
+              />
 
-            <date-picker-menu
-              label="Pickup By Date"
-              @date="pickupBy = $event"
-            />
+              <date-picker-menu
+                label="Pickup By Date"
+                @date="pickupBy = $event"
+                spacing="pb-3"
+              />
 
-            <v-checkbox v-model="urgent" label="Urgent?" hide-details />
+              <v-checkbox v-model="urgent" label="Urgent?" hide-details />
 
-            <conditional-date
-              question="Has the pickup date been confirmed?"
-              label="Confirmed Pickup Date"
-              @date="confirmedPickupDate = $event"
-            />
+              <conditional-date
+                question="Has the pickup date been confirmed?"
+                label="Confirmed Pickup Date"
+                @date="confirmedPickupDate = $event"
+              />
 
-            <conditional-date
-              question="Has the furniture been collected?"
-              label="Date Collected"
-              @date="dateCollected = $event"
-            />
+              <conditional-date
+                question="Has the furniture been collected?"
+                label="Date Collected"
+                @date="dateCollected = $event"
+              />
 
-            <conditional-date
-              question="Has the furniture been delivered?"
-              label="Date Delivered"
-              @date="dateDelivered = $event"
-            />
+              <conditional-date
+                question="Has the furniture been delivered?"
+                label="Date Delivered"
+                @date="dateDelivered = $event"
+                class="pb-3"
+              />
 
-            <v-divider class="my-3" />
+              <v-divider class="my-3" />
 
-            <h2>Images</h2>
+              <h2>Images</h2>
 
-            <!-- TODO: think of some way to host images -->
-            <p>Feature in development...</p>
+              <!-- TODO: think of some way to host images -->
+              <p>Feature in development...</p>
 
-            <v-divider class="my-3" />
+              <v-divider class="my-3" />
 
-            <h2>Comments</h2>
-            <v-textarea v-model="comments" label="Comments" auto-grow filled />
-
-            <div v-if="isStaff">
-              <h2>Staff Notes</h2>
+              <h2>Comments</h2>
               <v-textarea
-                v-model="staffNotes"
-                label="Staff Notes"
+                v-model="comments"
+                label="Comments"
                 auto-grow
                 filled
               />
-            </div>
-          </v-form>
-        </v-col>
-      </v-row>
-      <!-- </v-container> -->
+
+              <div v-if="isStaff">
+                <h2>Staff Notes</h2>
+                <v-textarea
+                  v-model="staffNotes"
+                  label="Staff Notes"
+                  auto-grow
+                  filled
+                />
+              </div>
+            </v-form>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-divider />
     </v-card-text>
 
     <v-card-actions>
@@ -181,6 +199,12 @@ export default class EditCard extends Vue {
 
   @Prop({ default: false })
   isStaff!: boolean;
+
+  offsetTop = 0;
+
+  onScroll(e: any): void {
+    this.offsetTop = e.target.scrollTop;
+  }
 
   valid = true;
 
@@ -247,7 +271,7 @@ export default class EditCard extends Vue {
 
   staffNotes = "";
 
-  updateAttr(attr: string, value: boolean) {
+  updateAttr(attr: string, value: boolean): void {
     switch (attr) {
       case "partsIntact":
         this.partsIntact = value;
@@ -279,7 +303,24 @@ export default class EditCard extends Vue {
 
 <style lang="scss" scoped>
 h2 {
-  margin-top: 1rem;
-  margin-bottom: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 1.5rem;
+
+  &:first-of-type {
+    padding-top: 0;
+  }
+}
+
+.ease-transition {
+  z-index: 1;
+  transition: box-shadow ease-in-out 0.1s;
+}
+
+.line {
+  border-bottom: 1px solid lightgray;
+}
+
+.no-line {
+  border-bottom: none;
 }
 </style>
