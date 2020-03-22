70980d481ce672498edc50a3ac27d4479449aaae
Catie Rencricca <catierencricca@gmail.com>
2020-03-21 21:48:12 -0400

    Implement skeleton class for filter system

--- src/plugins/MultiFilter.ts
+++ src/plugins/MultiFilter.ts
@@ -1,77 +1,105 @@
 // import Vue from "vue";
 // import Component from "vue-class-component";
-// import { FClass, Material, Status, Furniture } from "@/data/Furniture";
-// /**
-//  * Enabled v-data-table to have moire than one filter.
-//  */
-// class MultiFilters {
-//   items: Furniture[];
-//   filters: string[];
-//   filter: string;
-//   headers;
-//   filterCallbacks;
-//   filteredItems: Furniture[];
-//   self;
+import { Furniture } from "@/data/Furniture";
 
-//   /**
-//    * Constructor.
-//    *
-//    * @param items
-//    * @param filters
-//    * @param filter
-//    * @param headers
-//    */
-//   constructor(items, filters, filter, headers) {
-//     this.items = items;
-//     this.filter = filter;
-//     this.headers = headers;
-//     this.filters = filters;
-//     this.filterCallbacks = {};
-//   }
+/**
+ * Enabled v-data-table to have more than one filter.
+ */
+class MultiFilters {
+  // TODO: Undo filters individually
+  private inventory: Furniture[] = [];
+  private filters: number[] = [];
+  private vals: string[] = [];
+  private filterTemp: Furniture[] = [];
+  public filteredItems: Furniture[] = [];
+  /**
+   * Constructor.
+   *
+   * @param items the full inventory
+   * @param filters the list of filters being applied
+   * @param val the list of filter values to be matched against
+   */
+  public constructor(
+    inventory: Furniture[],
+    filters: number[],
+    vals: string[],
+  ) {
+    this.inventory = inventory;
+    this.filters = filters;
+    this.vals = vals;
+  }
 
-//   /**
-//    * Updates filter values.
-//    * @param filters filter´s object
-//    * @param val JSON chunk to be updated.
-//    * @returns {*}
-//    */
-//   static updateFilters(filters, val) {
-//     return Object.assign({}, filters, val);
-//   }
+  /* TODO: Stub for search */
+  private filter0(inv: Furniture[], filterVal: string): Furniture[] {
+    inv;
+    filterVal;
+    alert("stub");
+    return this.inventory;
+  }
 
-//   /**
-//    * Adds a new filter
-//    * @param filterName The name of the filter from which the information will be extracted
-//    * @param filterCallback The callback that will apply the filter.
-//    */
-//   registerFilter(filterName, filterCallback) {
-//     this.filterCallbacks[filterName] = filterCallback;
-//   }
+  /* TODO: Stub for class */
+  private filter1(inv: Furniture[], filterVal: string): Furniture[] {
+    let temp: Furniture[] = [];
+    for (let i = 0; i < inv.length; i++) {
+      if (inv[i].physical.class.toLowerCase().trim() === filterVal)
+        this.filterTemp.push(inv[i]);
+    }
+    return temp;
+  }
 
-//   /**
-//    * Run all filters.
-//    * @returns {*}
-//    */
-//   runFilters() {
-//     const self = this;
-//     this.filteredItems = self.items;
+  /* TODO: Stub for status */
+  private filter2(inv: Furniture[], filterVal: string): Furniture[] {
+    inv;
+    filterVal;
+    alert("stub");
+    return this.inventory;
+  }
 
-//     Object.entries(this.filterCallbacks).forEach(([entity, cb]) => {
-//       this.filteredItems = cb.call(
-//         self,
-//         self.filters[entity],
-//         this.filteredItems
-//       );
-//     });
+  /* Identify and execute the appropriate filtering function */
+  private selectFilter(
+    inv: Furniture[],
+    filterNum: number,
+    val: string,
+  ): Furniture[] {
+    if (filterNum === 0) return this.filter0(inv, val);
+    else if (filterNum === 1) return this.filter1(inv, val);
+    else if (filterNum === 2) return this.filter2(inv, val);
+    else return [];
+  }
 
-//     return this.filteredItems;
-//   }
-// }
+  /* Returns the filtered inventory based on given filter type and comparison value
+   * - FILTER 0: search
+   * - FILTER 1: class
+   * - FILTER 2: status
+   */
+  private multifilters(
+    inv: Furniture[],
+    filterList: number[],
+    vals: string[],
+  ): Furniture[] {
+    if (filterList.length >= 1) {
+      this.filterTemp = this.selectFilter(inv, filterList[0], vals[0]);
+    }
+    filterList.shift();
+    vals.shift();
+    if (filterList !== undefined && vals !== undefined) {
+      return this.multifilters(this.filterTemp, filterList, vals);
+    }
+    return this.filterTemp;
+  }
 
-// export default class MultiFiltersPlugin extends MultiFilters {
-//   install(Vue, options) {
-//     Vue.prototype.$MultiFilters = MultiFilters;
-//   }
-// }
+  /**
+   * Run all filters.
+   * @returns {*}
+   */
+  public runFilters(filterList: number[], vals: string[]): Furniture[] {
+    if (filterList.length === 1) {
+      return this.selectFilter(this.inventory, filterList[0], vals[0]);
+    } else {
+      this.filteredItems = this.multifilters(this.inventory, filterList, vals);
+      return this.filteredItems;
+    }
+  }
+}
 
-// // Vue.use(MultiFiltersPlugin);
+export default MultiFilters;
