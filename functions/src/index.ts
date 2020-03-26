99225a0b699141b95f0a0a359392145e3f596e0f
Ray Weng <rayrayweng@gmail.com>
2020-03-26 02:10:35 -0400

    Changed structure of index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,39 +5,41 @@ import functions = require("firebase-functions");
 
 admin.initializeApp();
 
-exports.getInventoryXLSX = functions.https.onCall(
-  async (data: { id: string[] }) => {
-    // const fs = require("fs");
-    const inventory: any = [];
-    const furniture = admin.firestore().collection("furniture");
-    const wb = XLSX.utils.book_new();
+async function getData(id: string[]): Promise<string> {
+  // const fs = require("fs");
+  const inventory: any = [];
+  const furniture = admin.firestore().collection("furniture");
+  const wb = XLSX.utils.book_new();
 
-    furniture
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          const item = doc.data();
-          // const itemInfo = [];
-          inventory.push(item);
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log(data.id.length);
+  furniture
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        const item = doc.data();
+        // const itemInfo = [];
+        inventory.push(item);
       });
-    wb.SheetNames.push("Inventory");
-    const inventoryWS = XLSX.utils.aoa_to_sheet(inventory);
-    wb.Sheets.Inventory = inventoryWS;
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log(id.length);
+    });
+  wb.SheetNames.push("Inventory");
+  const inventoryWS = XLSX.utils.aoa_to_sheet(inventory);
+  wb.Sheets.Inventory = inventoryWS;
 
-    const buffer = XLSX.write(wb, { bookType: "xlsx", type: "buffer" });
-    const storage = admin.storage();
-    const bucket = storage.bucket("inventory");
-    const today = new Date();
-    const date = `${today.getFullYear()}-${today.getMonth() +
-      1}-${today.getDate()}`;
-    const fileName = `${date}.xlsx`;
-    const file = bucket.file(fileName);
-    await file.save(buffer);
-    return `/inventory/${fileName}`;
-  },
-);
+  const buffer = XLSX.write(wb, { bookType: "xlsx", type: "buffer" });
+  const storage = admin.storage();
+  const bucket = storage.bucket("inventory");
+  const today = new Date();
+  const date = `${today.getFullYear()}-${today.getMonth() +
+    1}-${today.getDate()}`;
+  const fileName = `${date}.xlsx`;
+  const file = bucket.file(fileName);
+  await file.save(buffer);
+  return `/inventory/${fileName}`;
+}
+
+exports.getInventoryXLSX = functions.https.onCall((data: { id: string[] }) => {
+  return getData(data.id);
+});
