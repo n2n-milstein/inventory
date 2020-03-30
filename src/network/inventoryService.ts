912e28535fb8ab16d5da24162d551984a8eb6ace
Andrew <kungpaogao@gmail.com>
2020-03-30 00:23:37 -0400

    Implemented service to add, update, delete items

---
+++ src/network/inventoryService.ts
@@ -0,0 +1,36 @@
+import { Furniture } from "@/data/Furniture";
+import db from "./db";
+import Collections from "./collections";
+
+/**
+ * Adds an item to Firestore, setting its `id` and `dateAdded`.
+ * @param item - item to add to collection in Firestore
+ */
+export function addItem(item: Furniture): Promise<void> {
+  const data = { ...item };
+  const ref = db.collection(Collections.INVENTORY).doc();
+  data.id = ref.id;
+  data.timing.dateAdded = new Date();
+  return ref.set(data);
+}
+
+/**
+ * Updates an item in Firestore with specified properties of the `Furniture`
+ * class.
+ * @param id - id of the item to update
+ * @param updates - Partial type of furniture of the updates
+ */
+export function updateItem(
+  id: string,
+  updates: Partial<Furniture>,
+): Promise<void> {
+  return db.collection(Collections.INVENTORY).doc(id).update(updates);
+}
+
+/**
+ * Deletes an item in Firestore with specified `id`.
+ * @param id - id of the item to delete
+ */
+export function deleteItem(id: string): Promise<void> {
+  return db.collection(Collections.INVENTORY).doc(id).delete();
+}
