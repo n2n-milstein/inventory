fffbd0371434545bfdba0c308a264982a2a41844
Andrew <kungpaogao@gmail.com>
2020-04-01 15:05:44 -0400

    Added mutations for current and selected items

--- src/store/inventory/mutations.ts
+++ src/store/inventory/mutations.ts
@@ -1,9 +1,34 @@
 import { MutationTree } from "vuex";
+import { Furniture } from "@/data/Furniture";
 import { InventoryState, types } from "./types";
 
 const mutations: MutationTree<InventoryState> = {
   [types.CLEAR_INVENTORY](state: InventoryState): void {
     state.inventory = [];
+  },
+  [types.SET_CURRENT](
+    state: InventoryState,
+    { item }: { item: Furniture },
+  ): void {
+    state.current = item;
+  },
+  [types.CLEAR_CURRENT](state: InventoryState): void {
+    state.current = undefined;
+  },
+  [types.SET_SELECTED](
+    state: InventoryState,
+    { list }: { list: Furniture[] },
+  ): void {
+    state.selected = list;
+  },
+  [types.CLEAR_SELECTED](state: InventoryState): void {
+    state.selected = [];
+  },
+  [types.ADD_SELECTED](
+    state: InventoryState,
+    { item }: { item: Furniture },
+  ): void {
+    state.selected.push(item);
   },
 };
 
