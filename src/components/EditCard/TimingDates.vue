e1f552643e758464ed571a53d9536e30fd829394
Andrew <kungpaogao@gmail.com>
2020-04-08 23:06:48 -0400

    Initialize form component for handling timing data

---
+++ src/components/EditCard/TimingDates.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import Timing from "@/data/furniture/Timing";
+
+@Component
+export default class TimingDates extends Vue {
+  @Prop()
+  readonly value!: Timing;
+}
+</script>
