32a906cc7bef24ee6c491b119480370620587cdb
Andrew <kungpaogao@gmail.com>
2020-04-21 23:49:11 -0400

    Added v-model to ConditionalDate component
    - value is the date itself, stored as a *string*
    - selection of box is only selected when there is a date present
    - initialized to current date

--- src/components/EditCard/ConditionalDate.vue
+++ src/components/EditCard/ConditionalDate.vue
@@ -3,19 +3,15 @@
     <v-checkbox
       v-model="selected"
       :label="question"
-      @change="
-        {
-          if (!selected) $emit('date', '');
-        }
-      "
       hide-details
       :readonly="readonly"
     />
     <div v-if="selected" class="pt-1">
       <date-picker-menu
+        :value="value"
         :label="label"
-        @date="$emit('date', $event)"
         :readonly="readonly"
+        @input="updateDate($event)"
       />
     </div>
   </div>
@@ -28,6 +24,22 @@ import DatePickerMenu from "./DatePickerMenu.vue";
 
 @Component({ components: { DatePickerMenu } })
 export default class ConditionalDate extends Vue {
+  @Prop()
+  readonly value!: string;
+
+  updateDate(date: string): void {
+    this.$emit("input", date);
+  }
+
+  get selected(): boolean {
+    return !!this.value;
+  }
+
+  set selected(select: boolean) {
+    if (!select) this.updateDate("");
+    else this.updateDate(new Date().toLocaleDateString());
+  }
+
   @Prop({ default: "Default?" })
   readonly question!: string;
 
@@ -36,7 +48,5 @@ export default class ConditionalDate extends Vue {
 
   @Prop({ default: false })
   readonly readonly!: boolean;
-
-  selected = this.readonly;
 }
 </script>
