6e45f9f3179287785b0e87040ad4610099340b0e
Andrew <kungpaogao@gmail.com>
2020-05-31 02:28:28 -0400

    Implement archiveItem function in InventoryService
    - gets the document
    - deletes it from inventory
    - adds it to archive

--- src/network/inventory-service.ts
+++ src/network/inventory-service.ts
@@ -1,6 +1,31 @@
 import collections from "./collections";
 import FirestoreService from "./firestore-service";
+import db from "./db";
 
-const InventoryService = new FirestoreService(collections.INVENTORY);
+export class InventoryService extends FirestoreService {
+  archiveItem = async (id: string): Promise<void> => {
+    try {
+      const doc = await db.collection(this.collection).doc(id).get();
+      if (doc.exists) {
+        const item = doc.data() as any;
+        await db.collection(this.collection).doc(id).delete();
+        return db
+          .collection(collections.ARCHIVE)
+          .doc(id)
+          .set({ ...item });
+      }
+    } catch (e) {
+      console.log("Error archiving item: ", e);
+    }
+    return new Promise(() => {
+      // do nothing
+    });
+  };
+}
 
-export const { addItem, updateItem, deleteItem } = InventoryService;
+export const {
+  addItem,
+  updateItem,
+  deleteItem,
+  archiveItem,
+} = new InventoryService(collections.INVENTORY);
