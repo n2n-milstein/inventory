67f3707eae6b76142f2ab3cdd88af67d92ee9ea2
Andrew <kungpaogao@gmail.com>
2020-06-04 21:41:39 -0400

    Add unarchiveItem to archive service
    - fixed some code in inventory service to match

--- src/network/archive-service.ts
+++ src/network/archive-service.ts
@@ -1,6 +1,32 @@
+import { Furniture } from "@/data/Furniture";
 import collections from "./collections";
 import FirestoreService from "./firestore-service";
+import db from "./db";
 
-const ArchiveService = new FirestoreService(collections.ARCHIVE);
+class ArchiveService extends FirestoreService {
+  unarchiveItem = async (id: string): Promise<void> => {
+    try {
+      const doc = await db.collection(this.collection).doc(id).get();
+      if (doc.exists) {
+        const item = doc.data() as Furniture;
+        await db.collection(this.collection).doc(id).delete();
+        return db
+          .collection(collections.INVENTORY)
+          .doc(id)
+          .set({ ...item });
+      }
+    } catch (e) {
+      console.log("Error unarchiving item: ", e);
+    }
+    return new Promise(() => {
+      // do nothing
+    });
+  };
+}
 
-export const { addItem, updateItem, deleteItem } = ArchiveService;
+export const {
+  addItem,
+  updateItem,
+  deleteItem,
+  unarchiveItem,
+} = new ArchiveService(collections.ARCHIVE);
--- src/network/inventory-service.ts
+++ src/network/inventory-service.ts
@@ -1,13 +1,14 @@
+import { Furniture } from "@/data/Furniture";
 import collections from "./collections";
 import FirestoreService from "./firestore-service";
 import db from "./db";
 
-export class InventoryService extends FirestoreService {
+class InventoryService extends FirestoreService {
   archiveItem = async (id: string): Promise<void> => {
     try {
       const doc = await db.collection(this.collection).doc(id).get();
       if (doc.exists) {
-        const item = doc.data() as any;
+        const item = doc.data() as Furniture;
         await db.collection(this.collection).doc(id).delete();
         return db
           .collection(collections.ARCHIVE)
