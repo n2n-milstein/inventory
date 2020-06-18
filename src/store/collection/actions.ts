021b06c713e8168bbc3c5f06db3d53c9cb609b7b
Andrew <kungpaogao@gmail.com>
2020-06-18 15:46:21 -0400

    Implement actions for export current and selected

--- src/store/collection/actions.ts
+++ src/store/collection/actions.ts
@@ -15,6 +15,14 @@ const actions: ActionTree<CollectionState, RootState> = {
     commit(mutation.CLEAR_UPDATES);
     commit(mutation.CLEAR_CURRENT);
   },
+  async [action.EXPORT_CURRENT]({ state }): Promise<void> {
+    try {
+      const service = new FirestoreService(state.collection!);
+      await service.exportItems([state.current!.id]);
+    } catch (e) {
+      console.error("exportCurrent error:", e);
+    }
+  },
   [action.UPDATE_CURRENT](
     { commit },
     { updates }: { updates: Partial<Furniture> },
@@ -29,13 +37,14 @@ const actions: ActionTree<CollectionState, RootState> = {
     { collection }: { collection: collections },
   ): Promise<void> {
     try {
+      // TODO: just use state.collection
       const service = new FirestoreService(collection);
       commit(mutation.UPDATE_CURRENT, { updates: state.currentUpdates });
       commit(mutation.CLEAN_CURRENT);
       await service.updateItem(state.current!.id, state.currentUpdates);
       commit(mutation.CLEAR_UPDATES);
     } catch (e) {
-      console.log("commitUpdates error: ", e);
+      console.error("commitUpdates error:", e);
     }
   },
   [action.SET_SELECTED]({ commit }, { list }: { list: Furniture[] }): void {
@@ -46,6 +55,14 @@ const actions: ActionTree<CollectionState, RootState> = {
   },
   [action.ADD_SELECTED]({ commit }, { item }: { item: Furniture }): void {
     commit(mutation.ADD_SELECTED, { item });
+  },
+  async [action.EXPORT_SELECTED]({ state }): Promise<void> {
+    try {
+      const service = new FirestoreService(state.collection!);
+      await service.exportItems(state.selected.map((item) => item.id));
+    } catch (e) {
+      console.error("exportSelected error:", e);
+    }
   },
   [action.BIND_ITEMS]: firestoreAction<CollectionState, RootState>(
     ({ bindFirestoreRef, state }) => {
