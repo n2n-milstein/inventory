e4f9424917af16e31f7cf3eca30d6b2747f5c276
Andrew <kungpaogao@gmail.com>
2020-06-18 14:23:30 -0400

    Add exports for other Firebase services

--- src/network/db.ts
+++ src/network/db.ts
@@ -1,7 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/functions";
+import "firebase/storage";
 import config from "./config";
 
-const db = firebase.initializeApp(config).firestore();
+firebase.initializeApp(config);
+
+export const db = firebase.firestore();
+
+export const functions = firebase.functions();
+
+export const storage = firebase.storage();
 
 export default db;
