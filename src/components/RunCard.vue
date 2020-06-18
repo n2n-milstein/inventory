5b326e878fed3209680d38628d82d9a70af57bd9
kira-segenchuk <relatively.kira@gmail.com>
2020-06-18 16:12:54 -0400

    working on making runs card

---
+++ src/components/RunCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-list-item><v-lits-item-title>Volunteers</v-lits-item-title></v-list-item>
+    <v-list-item><v-lits-item-title>Pick Ups</v-lits-item-title></v-list-item>
+    <v-list-item><v-lits-item-title>Drop Offs</v-lits-item-title></v-list-item>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import EditCard from "@/components/EditCard.vue";
+
+@Component({ components: { EditCard } })
+export default class RunCard extends Vue {
+  @Prop({})
+  readonly title!: string;
+
+  @Prop({})
+  readonly volunteers!: any;
+
+  @Prop({})
+  readonly pickUps!: any;
+
+  @Prop({})
+  readonly dropOffs!: any;
+}
+</script>
--- src/views/RunsPage.vue
+++ src/views/RunsPage.vue
@@ -1,6 +1,7 @@
 <template>
   <v-col cols="12">
     <furniture-table-header v-model="search" title="Runs" />
+    <run-card title="Date" volunteers="" pick-ups="" drop-offs="" />
   </v-col>
 </template>
 
@@ -12,7 +13,8 @@ import "firebase/functions";
 import "firebase/storage";
 // components
 import FurnitureTableHeader from "@/components/FurnitureTableHeader.vue";
+import RunCard from "@/components/RunCard.vue";
 
-@Component({ components: { FurnitureTableHeader } })
-export default class Inventory extends Vue {}
+@Component({ components: { FurnitureTableHeader, RunCard } })
+export default class RunsPage extends Vue {}
 </script>
