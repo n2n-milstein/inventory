2f2e9730e6b4d7bd7eaf0498d0c64c0f29b6f875
Andrew <kungpaogao@gmail.com>
2020-07-03 00:25:32 -0400

    Prep runs data model to store denormalized data

--- src/data/Run.ts
+++ src/data/Run.ts
@@ -1,4 +1,5 @@
 import { Timestamp } from "./furniture/Timing";
+import { Furniture } from "./Furniture";
 
 export enum RunStatus {
   Planning,
@@ -9,6 +10,8 @@ export enum RunStatus {
 
 export default class Run {
   // metadata
+  public id: string;
+
   public dateCreated: Date | Timestamp;
 
   public lastUpdated: Date | Timestamp;
@@ -18,21 +21,24 @@ export default class Run {
 
   public volunteers: string[]; // list of volunteer IDs
 
-  public pickups: string[]; // list of furniture IDs
+  public pickups: Furniture[]; // list of furniture
 
   // list of client ID, furniture ID pairs
-  public dropoffs: { client: string; furniture: string }[];
+  // public dropoffs: { client: string; furniture: Furniture }[];
+  public dropoffs: Furniture[];
 
   public status: RunStatus;
 
   public constructor(
+    id = "",
     date = new Date(),
     volunteers = [] as string[],
-    pickups = [] as string[],
+    pickups = [] as Furniture[],
     dropoffs = [],
     status = RunStatus.Planning,
   ) {
     // metadata
+    this.id = id;
     this.dateCreated = new Date();
     this.lastUpdated = new Date();
 
