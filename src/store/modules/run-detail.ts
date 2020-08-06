1c50b348a0aacca4d7f6f249d2e441902712a1b2
Andrew <kungpaogao@gmail.com>
2020-08-06 16:16:43 -0400

    Implement state, actions for binding, updating run

--- src/store/modules/run-detail.ts
+++ src/store/modules/run-detail.ts
@@ -1,10 +1,15 @@
 import { Furniture } from "@/data/Furniture";
 import Run from "@/data/Run";
-import { getItem } from "@/network/run-service";
+import { getItem, updateItem } from "@/network/run-service";
+import { firestoreAction } from "vuexfire";
+import collections from "@/network/collections";
+import { db } from "@/network/firebase";
+import { cleanUndefined } from "@/network/converters";
 
 export const runDetailModule = {
   namespaced: true,
   state: () => ({
+    id: "",
     run: null,
     runUpdates: null,
     furniture: null,
@@ -19,18 +24,30 @@ export const runDetailModule = {
       state.furnitureUpdates ? Object.keys(state.furnitureUpdates).length : 0,
   },
   mutations: {
+    /** run */
+    SET_ID(state: any, { id }: { id: string }): void {
+      state.id = id;
+    },
     SET_RUN(state: any, { run }: { run: Run }): void {
       state.run = run;
     },
     UPDATE_RUN(state: any): void {
       state.run = state.run ? { ...state.run, ...state.runUpdates } : null;
     },
+    CLEAN_RUN(state: any): void {
+      if (state.run === null) return;
+      state.run = cleanUndefined(state.run);
+    },
+
+    /** run updates */
     ADD_RUN_UPDATES(state: any, { updates }: { updates: Partial<Run> }): void {
       state.runUpdates = { ...state.runUpdates, ...updates };
     },
     CLEAR_RUN_UPDATES(state: any): void {
       state.runUpdates = null;
     },
+
+    /** furniture */
     SET_FURNITURE(state: any, { item }: { item: Furniture }): void {
       state.furniture = item;
     },
@@ -42,6 +59,8 @@ export const runDetailModule = {
         ? { ...state.furniture, ...state.furnitureUpdates }
         : null;
     },
+
+    /** furniture updates */
     ADD_FURNITURE_UPDATES(
       state: any,
       { updates }: { updates: Partial<Furniture> },
@@ -53,6 +72,9 @@ export const runDetailModule = {
     },
   },
   actions: {
+    setId({ commit }: any, { id }: { id: string }): void {
+      commit("SET_ID", { id });
+    },
     async getRun({ commit }: any, { id }: { id: string }): Promise<void> {
       try {
         const run = await getItem(id);
@@ -76,6 +98,27 @@ export const runDetailModule = {
     // ): void {
     //   commit("ADD_FURNITURE_UPDATES", { updates });
     // },
+    updateRun({ commit }: any, { updates }: { updates: Partial<Run> }): void {
+      commit("ADD_RUN_UPDATES", { updates });
+    },
+    async commitRunUpdates({ commit, state }: any): Promise<void> {
+      if (Object.keys(state.runUpdates).length === 0) return;
+
+      try {
+        commit("UPDATE_RUN");
+        commit("CLEAN_RUN");
+        await updateItem(state.run.id, state.runUpdates);
+        commit("CLEAR_RUN_UPDATES");
+      } catch (err) {
+        console.error("commitRunUpdates", err);
+      }
+    },
+    bindRun: firestoreAction(({ bindFirestoreRef, state }) => {
+      return bindFirestoreRef(
+        "run",
+        db.collection(collections.RUNS).doc((state as any).id),
+      );
+    }),
   },
 };
 
